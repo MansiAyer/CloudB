f8068d87e9dd93c316b25d7a1f2a975b84dd84d2
Mansi Ayer <42694815+MansiAyer@users.noreply.github.com>
2022-04-11 03:01:33 +0530

    rds fix 2

--- file/connection.php
+++ file/connection.php
@@ -1,8 +1,8 @@
 <?php
- include "../inc/dbinfo.inc";
+ include "../../inc/dbinfo.inc";
 
  /* Connect to MySQL and select the database. */
-  $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, 3306);
+  $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
   if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
